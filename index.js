69ec81e3c6351cc70ab1ac3385de4e4a569fcf9b
José Luis Olivares <joseluis_503@yahoo.com>
2016-11-19 13:59:24 -0600

    Update index.js
    Cambios en index js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express=require('express');
 var app=express();
-var http=require('http').server(app);
+var http=require('http').Server(app);
 //usando socketio
 var io=require('socket.io')(http);
 
