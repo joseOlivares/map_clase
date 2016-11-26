c3867c4ebe08768cedbfea285da196c28b069bed
José Luis Olivares <joseluis_503@yahoo.com>
2016-11-26 10:33:57 -0600

    Add files via upload
    Server configurado para enviar contador a los clientes

--- index.js
+++ index.js
@@ -4,14 +4,31 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 
-app.use(express.static(__dirname + '/public')); //sirviendo archivos estaticos como  css, js, images
+app.use(express.static(__dirname + '/public')); //serving statics files like css, js, images
 
-var port=process.env.PORT || 3000; //definiendo el puerto para heroku
+var port=process.env.PORT || 3000; //this is for heroku
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-}); 
 
+  res.sendFile(__dirname + '/index.html');
+
+});
 http.listen(port,function(){
     console.log('Escuchando en el puerto: '+port);//mensaje en consola 
+});
+
+
+var socketCount=0;//contador de conexiones al server
+
+io.on('connection',function(socket){
+		console.log('Usuario conectado...');
+		socketCount++;
+		//emitiendo mensaje a TODOS los socketsno usuarios conetados
+		io.sockets.emit('usuario conectado',socketCount);
+});//cierra conexion
+
+socket.on('disconnect',function(){
+	socketCount--;//decremento del contador
+	console.log('Usuario Desconectado');//menaje en consola
+	io.sockets.emit('usuario desconectado');//mensaje a todos los sockets
 });
